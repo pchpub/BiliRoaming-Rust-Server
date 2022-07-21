cd667cce2bdf3865930f3d471884ba945e7ddfcd
pchpub <73123406+pchpub@users.noreply.github.com>
2022-07-21 13:23:32 +0800

    support json config

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,12 @@ use actix_web::{http::header::ContentType};
 use qstring::QString;
 use md5;
 use chrono::prelude::*;
+use std::fs::File;
+use serde_json;
+use serde::{Deserialize, Serialize};
 
 async fn get_playurl(req: &HttpRequest,is_app: bool) -> impl Responder {
-    let pool = req.app_data::<Pool>().unwrap();
+    let (pool,config) = req.app_data::<(Pool,BiliConfig)>().unwrap();
     match req.headers().get("user-agent") {
         Option::Some(_ua) => (),
         _ => {
@@ -29,13 +32,14 @@ async fn get_playurl(req: &HttpRequest,is_app: bool) -> impl Responder {
     }
     };
 
-    let appkey = match query.get("appkey") {
+    let mut appkey = match query.get("appkey") {
         Option::Some(key) => key,
-        _ => {
-            return HttpResponse::Ok()
-                .content_type(ContentType::plaintext())
-                .body("{\"code\":-2333,\"message\":\"差不多得了,appkey都没\"}");
-        }
+        _ => "1d8b6e7d45233436", //为了应对新的appkey,应该设定默认值
+        // _ => {
+        //     return HttpResponse::Ok()
+        //         .content_type(ContentType::plaintext())
+        //         .body("{\"code\":-2333,\"message\":\"差不多得了,appkey都没\"}");
+        // }
     };
 
     let area = match query.get("area") {
@@ -76,7 +80,7 @@ async fn get_playurl(req: &HttpRequest,is_app: bool) -> impl Responder {
         }
     };
 
-    let appsec = match appkey_to_sec(appkey){
+    let mut appsec = match appkey_to_sec(appkey){
         Ok(value) => value,
         Err(()) => {
             return HttpResponse::Ok()
@@ -84,12 +88,13 @@ async fn get_playurl(req: &HttpRequest,is_app: bool) -> impl Responder {
                 .body("{\"code\":-2336,\"message\":\"未知设备\"}");
         }
     };
+
     let user_info = match getuser_list(pool, access_key, appkey, &appsec,&user_agent).await{
         Ok(value)=> value,
-        Err(()) => {
+        Err(value) => {
             return HttpResponse::Ok()
                 .content_type(ContentType::plaintext())
-                .body("{\"code\":-2337,\"message\":\"获取用户信息失败\"}");
+                .body(format!("{{\"code\":-2337,\"message\":\"{value}\"}}"));
         }
     };
     
@@ -151,8 +156,11 @@ async fn get_playurl(req: &HttpRequest,is_app: bool) -> impl Responder {
     if is_expire {
         //println!("is_expire");
         let base_url = match area_num {
-            1 => "https://api.bilibili.com.proxy.pch.pub",
-            _ => "https://api.bilibili.com",
+            1 => &config.cn_host,
+            2 => &config.hk_host,
+            3 => &config.tw_host,
+            4 => &config.th_host,
+            _ => &config.th_host,
         };
         let ts_string = ts.to_string();
         let mut query_vec = vec![
@@ -169,21 +177,54 @@ async fn get_playurl(req: &HttpRequest,is_app: bool) -> impl Responder {
             ("ts",&ts_string),
         ];
         if cid_open {
-            query_vec.insert(3, ("cid",&cid));
+            query_vec.push(("cid",&cid));
             //add_pair(("cid",query.get("device").unwrap_or("114514")));
         }
+        match area_num {
+            4 => {
+                appkey = "7d089525d3611b1c";
+                appsec = appkey_to_sec(&appkey).unwrap();
+                query_vec.push(("s_locale","zh_SG"));
+            }
+            _ => (),
+        }
+        query_vec.sort_by_key(|v| v.0);
         let unsigned_url = qstring::QString::new(query_vec);
         let unsigned_url = format!("{unsigned_url}");
         let signed_url = format!("{unsigned_url}&sign={:x}",md5::compute(format!("{unsigned_url}{appsec}")));
         let proxy_open = match area_num {
-            1 => false,
-            2 => false,
-            _ => true,
+            1 => &config.cn_proxy_open,
+            2 => &config.hk_proxy_open,
+            3 => &config.tw_proxy_open,
+            4 => &config.th_proxy_open,
+            _ => &config.th_proxy_open,
         };
-        let proxy_url = "127.0.0.1:7890";
+        let proxy_url = match area_num{
+            1 => &config.cn_proxy_url,
+            2 => &config.hk_proxy_url,
+            3 => &config.tw_proxy_url,
+            4 => &config.th_proxy_url,
+            _ => &config.th_proxy_url,
+        };
         let api = match is_app {
-            true => "/pgc/player/api/playurl",
-            false => "/pgc/player/web/playurl",
+            true => {
+                match area_num{
+                    1 => &config.cn_app_playurl_api,
+                    2 => &config.hk_app_playurl_api,
+                    3 => &config.tw_app_playurl_api,
+                    4 => &config.th_app_playurl_api,
+                    _ => &config.th_app_playurl_api,
+                }
+            },
+            false => {
+                match area_num {
+                    1 => &config.cn_web_playurl_api,
+                    2 => &config.hk_web_playurl_api,
+                    3 => &config.tw_web_playurl_api,
+                    4 => &config.th_web_playurl_api,
+                    _ => &config.th_web_playurl_api,
+                }
+            },
         };
 
         let body_data = match getwebpage(&format!("{base_url}{api}?{signed_url}"), proxy_open, &proxy_url,&user_agent) {
@@ -207,7 +248,7 @@ async fn get_playurl(req: &HttpRequest,is_app: bool) -> impl Responder {
 }
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().body("Rust server is online!")
 }
 
 #[get("/pgc/player/api/playurl")]
@@ -217,6 +258,11 @@ async fn zhplayurl_app(req:HttpRequest) -> impl Responder {
 
 #[get("/pgc/player/web/playurl")]
 async fn zhplayurl_web(req:HttpRequest) -> impl Responder {
+    get_playurl(&req, false).await
+}
+
+#[get("/intl/gateway/v2/ogv/playurl")]
+async fn zh_search3(req:HttpRequest) -> impl Responder {
     get_playurl(&req, false).await
 }
 
@@ -240,14 +286,18 @@ async fn zhplayurl_web(req:HttpRequest) -> impl Responder {
 //     HttpResponse::Ok().body("Hello world!")
 // }
 
-fn getwebpage(url: &str,proxy_open: bool,proxy_url: &str,user_agent: &str) -> Result<String, ()> {
-    //println!("url:{url}");
 
+
+fn getwebpage(url: &str,proxy_open: &bool,proxy_url: &str,user_agent: &str) -> Result<String, ()> {
     let mut data = Vec::new();
     let mut handle = Easy::new();
     handle.url(url).unwrap();
-    if proxy_open { 
-        handle.proxy_type(curl::easy::ProxyType::Socks5).unwrap();
+    handle.follow_location(true).unwrap();
+    handle.ssl_verify_peer(false).unwrap();
+    handle.post(false).unwrap();
+    
+    if *proxy_open { 
+        handle.proxy_type(curl::easy::ProxyType::Socks5Hostname).unwrap();
         handle.proxy(proxy_url).unwrap();
         handle.useragent(user_agent).unwrap();
     }
@@ -327,25 +377,49 @@ async fn redis_set(redis: &Pool,key: &String,value: &String,expire_time: u64) ->
     Some(())
 } 
 
-async fn getuser_list(redis: &Pool,access_key: &str,appkey:&str,appsec:&str,user_agent: &str) -> Result<UserInfo,()> {
+async fn getuser_list(redis: &Pool,access_key: &str,appkey:&str,appsec:&str,user_agent: &str) -> Result<UserInfo,String> {
     let info: String = match redis_get(&redis,&format!("{access_key}20501")).await {
         Some(value) => value,
         None => {
             let dt = Local::now();
             let ts = dt.timestamp_millis() as u64;
-            let sign = md5::compute(format!("access_key={}&appkey={}&ts={}{}",access_key,appkey,ts,appsec));
-            let url:String = format!("https://app.bilibili.com/x/v2/account/myinfo?access_key={}&appkey={}&ts={}&sign={:x}",access_key,appkey,ts,sign);
-            let output = match getwebpage(&url,false,"",user_agent){
+            let ts_min = dt.timestamp() as u64;
+            let sign = md5::compute(format!("access_key={}&appkey={}&ts={}{}",access_key,appkey,ts_min,appsec));
+            let url:String = format!("https://app.bilibili.com/x/v2/account/myinfo?access_key={}&appkey={}&ts={}&sign={:x}",access_key,appkey,ts_min,sign);
+            //println!("{}",url);
+            let output = match getwebpage(&url,&false,"",user_agent){
                 Ok(data) => data,
-                Err(_) => {return Err(());}
+                Err(_) => {
+                    println!("getuser_list函数寄了 url:{}",url);
+                    return Err("emmmm解析服务器的网络问题".to_string());
+                }
             };
+            //println!("{}",output);
             let output_json = json::parse(&output).unwrap();
-            let output_struct = UserInfo{
-                access_key: String::from(access_key),
-                uid: output_json["data"]["mid"].as_u64().unwrap(),
-                vip_expire_time: output_json["data"]["vip"]["due_date"].as_u64().unwrap(),
-                expire_time: ts+25*24*60*60*1000,//用户状态25天强制更新
-            };
+            let output_struct: UserInfo;
+            if output_json["code"].as_i32().unwrap() == 0 {
+                output_struct = UserInfo{
+                    access_key: String::from(access_key),
+                    uid: output_json["data"]["mid"].as_u64().unwrap(),
+                    vip_expire_time: output_json["data"]["vip"]["due_date"].as_u64().unwrap(),
+                    expire_time: ts+25*24*60*60*1000,//用户状态25天强制更新
+                };
+            }else if output_json["code"].as_i32().unwrap() == -400{
+                println!("getuser_list函数寄了 output_json:{}",output_json);
+                return Err("可能你用的不是手机".to_string());
+            }else if output_json["code"].as_i32().unwrap() == -101{
+                println!("getuser_list函数寄了 output_json:{}",output_json);
+                return Err("账号未登录喵(b站api说的,估计你access_key过期了)".to_string());
+            }else if output_json["code"].as_i32().unwrap() == -3{
+                println!("getuser_list函数寄了 output_json:{}",output_json);
+                return Err("可能我sign参数算错了,非常抱歉喵".to_string());
+            }else if output_json["code"].as_i32().unwrap() == -412{
+                println!("getuser_list函数寄了 output_json:{}",output_json);
+                return Err("被草到风控了.....".to_string());
+            }else{
+                println!("getuser_list函数寄了 output_json:{}",output_json);
+                return Err(format!("鼠鼠说:{}",output_json["code"].as_i32().unwrap()));
+            }
             let key  = format!("{access_key}20501");
             let value = output_struct.to_json();
             let _ : () = redis_set(&redis,&key, &value,25*24*60*60).await.unwrap_or_default();
@@ -425,7 +499,7 @@ async fn getusercer_list(redis: &Pool,uid: &u64,access_key: &str) -> Result<User
     };
     
     if is_expire {
-        let getwebpage_data = match getwebpage(&format!("https://black.qimo.ink/status.php?access_key={access_key}"), false, "",""){
+        let getwebpage_data = match getwebpage(&format!("https://black.qimo.ink/status.php?access_key={access_key}"), &false, "",""){
             Ok(data) => data,
             Err(_) => {return Err(())}
         };
@@ -480,7 +554,7 @@ async fn auth_user(redis: &Pool,uid: &u64,access_key: &str) -> Result<(bool,bool
 fn appkey_to_sec(appkey:&str) -> Result<String, ()> {
 	match appkey {
         "9d5889cf67e615cd" => Ok("8fd9bb32efea8cef801fd895bef2713d".to_string()), // Ai4cCreatorAndroid
-		"1d8b6e7d45233436" => Ok("560c52ccd288fed045859ed18bffd973".to_string()), // Android
+		"1d8b6e7d45233436" => Ok("560c52ccd288fed045859ed18bffd973".to_string()), // Android 
 		"07da50c9a0bf829f" => Ok("25bdede4e1581c836cab73a48790ca6e".to_string()), // AndroidB
 		"8d23902c1688a798" => Ok("710f0212e62bd499b8d3ac6e1db9302a".to_string()), // AndroidBiliThings
 		"dfca71928277209b" => Ok("b5475a8825547a4fc26c7d518eaaa02e".to_string()), // AndroidHD
@@ -491,18 +565,68 @@ fn appkey_to_sec(appkey:&str) -> Result<String, ()> {
 		"37207f2beaebf8d7" => Ok("e988e794d4d4b6dd43bc0e89d6e90c43".to_string()), // BiliLink
 		"9a75abf7de2d8947" => Ok("35ca1c82be6c2c242ecc04d88c735f31".to_string()), // BiliScan
 		"7d089525d3611b1c" => Ok("acd495b248ec528c2eed1e862d393126".to_string()), // BstarA
+        "178cf125136ca8ea" => Ok("34381a26236dd1171185c0beb042e1c6".to_string()), // AndroidB
+        "27eb53fc9058f8c3" => Ok("c2ed53a74eeefe3cf99fbd01d8c9c375".to_string()), // ios
+        "57263273bc6b67f6" => Ok("a0488e488d1567960d3a765e8d129f90".to_string()), // Android
+        "7d336ec01856996b" => Ok("a1ce6983bc89e20a36c37f40c4f1a0dd".to_string()), // AndroidB
+        "85eb6835b0a1034e" => Ok("2ad42749773c441109bdc0191257a664".to_string()), // unknown
+        "8e16697a1b4f8121" => Ok("f5dd03b752426f2e623d7badb28d190a".to_string()), // AndroidI
+        "aae92bc66f3edfab" => Ok("af125a0d5279fd576c1b4418a3e8276d".to_string()), // PC	投稿工具
+        "ae57252b0c09105d" => Ok("c75875c596a69eb55bd119e74b07cfe3".to_string()), // AndroidI
+        "bca7e84c2d947ac6" => Ok("60698ba2f68e01ce44738920a0ffe768".to_string()), // login
+        "iVGUTjsxvpLeuDCf" => Ok("aHRmhWMLkdeMuILqORnYZocwMBpMEOdt".to_string()), //Android	取流专用
+        "YvirImLGlLANCLvM" => Ok("JNlZNgfNGKZEpaDTkCdPQVXntXhuiJEM".to_string()), //ios	取流专用
+        //_ => Ok("560c52ccd288fed045859ed18bffd973".to_string()),
         _ => Err(())
     }
 }
 
+#[derive(Serialize, Deserialize,Clone)]
+struct BiliConfig {
+    redis : String,
+    cn_host: String,
+    tw_host : String,
+    hk_host : String,
+    th_host : String,
+    cn_app_playurl_api : String,
+    tw_app_playurl_api : String,
+    hk_app_playurl_api : String,
+    th_app_playurl_api : String,
+    cn_web_playurl_api : String,
+    tw_web_playurl_api : String,
+    hk_web_playurl_api : String,
+    th_web_playurl_api : String,
+    cn_app_search_api : String,
+    tw_app_search_api : String,
+    hk_app_search_api : String,
+    th_app_search_api : String,
+    cn_web_search_api : String,
+    tw_web_search_api : String,
+    hk_web_search_api : String,
+    th_web_search_api : String,
+    cn_proxy_url : String,
+    tw_proxy_url : String,
+    hk_proxy_url : String,
+    th_proxy_url : String,
+    cn_proxy_open : bool,
+    tw_proxy_open : bool,
+    hk_proxy_open : bool,
+    th_proxy_open : bool,
+}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("你好喵~");
-    HttpServer::new(|| {
-        let rediscfg = Config::from_url("redis://default:********@127.0.0.1:6379");//明天写读取配置文件（
+    let config_file: File;
+    match File::open("config.json") {
+        Ok(value) => config_file = value,
+        Err(_) => {std::process::exit(78);},
+    }
+    let config: BiliConfig = serde_json::from_reader(config_file).unwrap();
+    HttpServer::new(move || {
+        let rediscfg = Config::from_url(&config.redis);//明天写读取配置文件（
         let pool = rediscfg.create_pool(Some(Runtime::Tokio1)).unwrap();
         App::new()
-            .app_data(pool)
+            .app_data((pool,config.clone()))
             .service(hello)
             .service(zhplayurl_app)
             .service(zhplayurl_web)
