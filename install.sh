edb182772b227bfcfe7fd13bd413a98c53c2ccc4
pchpub <73123406+pchpub@users.noreply.github.com>
2022-07-24 21:21:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ debian|ubuntu|devuan)
     sudo apt-get install redis
     ;;
 centos|fedora|rhel)
-    sudo yum install git
-    sudo yum install cargo
-    sudo yum install screen
-    sudo yum install redis
+    sudo yum install -y git
+    sudo yum install -y cargo
+    sudo yum install -y screen
+    sudo yum install -y redis
     ;;
 *)
     exit 1
@@ -28,5 +28,6 @@ sudo chmod 777 /root/rust/config.json
 cd /root/rust/
 echo "请去按实际情况修改/root/rust/config.json 修改好再来"
 read -p  "修改好了后按下任意键"
+
 screen -dmS "biliroaming_rust_server" ./biliroaming_rust_server
 echo "请反代到127.0.0.1:2662(这个端口就是config中的port,默认为2662)"
