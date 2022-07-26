4a7831888195205e412009b809d4edf45fac28ad
pchpub <73123406+pchpub@users.noreply.github.com>
2022-07-26 17:42:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,14 +24,6 @@ sudo chmod 777 /root/rust/config.json
 cd /root/rust/
 echo "请去按实际情况修改/root/rust/config.json 修改好再来"
 read -p  "修改好了后按下任意键"
-port=`grep \"port\" config.example.json | awk -F ':' '{print $2}'`
-pIDa=`/usr/sbin/lsof -i :8081|grep -v "PID" | awk '{print $2}'`
-echo $pIDa
-if [ "$pIDa" != "" ];
-then
-   echo "1"
-else
-   echo "0"
 
 screen -dmS "biliroaming_rust_server" ./biliroaming_rust_server
 echo "请反代到127.0.0.1:2662(这个端口就是config中的port,默认为2662)"
