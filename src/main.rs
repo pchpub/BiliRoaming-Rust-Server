9fcf1f5e0491e10dac968777135456e35c5ff7d0
pchpub <73123406+pchpub@users.noreply.github.com>
2022-08-15 21:00:17 +0800

    fix infinite spawn

--- src/main.rs
+++ src/main.rs
@@ -119,9 +119,10 @@ async fn thsubtitle_web(req: HttpRequest) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     println!("你好喵~");
     ctrlc::set_handler(move || {
-        println!("已关闭 biliroaming_rust_server");
+        println!("\n已关闭 biliroaming_rust_server");
         std::process::exit(0);
-    }).unwrap();
+    })
+    .unwrap();
     let config_file: File;
     let mut config_type: Option<&str> = None;
     let config_suffix = ["json", "yml"];
@@ -167,21 +168,24 @@ async fn main() -> std::io::Result<()> {
     let bilisender = Arc::new(s);
     let anti_speedtest_redis_cfg = Config::from_url(&config.redis);
     let pool_background = anti_speedtest_redis_cfg
-            .create_pool(Some(Runtime::Tokio1))
-            .unwrap();
+        .create_pool(Some(Runtime::Tokio1))
+        .unwrap();
     let web_background = actix_web::rt::spawn(async move {
         //a thread try to update cache
-        println!("1-创建");//Debug
         loop {
-            println!("1-循环前");//Debug
             let receive_data = match r.recv().await {
                 Ok(it) => it,
                 _ => continue,
             };
-            println!("1-循环中");//Debug
             match receive_data.data_type {
                 1 => {
-                    match get_playurl_background(&pool_background, &receive_data, &anti_speedtest_cfg).await {
+                    match get_playurl_background(
+                        &pool_background,
+                        &receive_data,
+                        &anti_speedtest_cfg,
+                    )
+                    .await
+                    {
                         Ok(_) => (),
                         Err(value) => println!("{value}"),
                     };
@@ -193,7 +197,7 @@ async fn main() -> std::io::Result<()> {
             }
         }
     });
-    
+
     let web_main = HttpServer::new(move || {
         let rediscfg = Config::from_url(&config.redis);
         let pool = rediscfg.create_pool(Some(Runtime::Tokio1)).unwrap();
