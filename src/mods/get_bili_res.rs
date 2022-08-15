5dd6392857dee302525e89a359af2736d2fd343d
pchpub <73123406+pchpub@users.noreply.github.com>
2022-08-15 14:00:42 +0800

    fix bug

--- src/mods/get_bili_res.rs
+++ src/mods/get_bili_res.rs
@@ -1274,10 +1274,10 @@ pub async fn errorurl_reg(url: &str) -> Option<u8>{
     } else {
         return None
     };
-    let caps = match re.captures(url.as_bytes()){
-        Ok(value) => value,
-        Err(_) => return None,
-    }.unwrap();
+    let caps = match re.captures(url.as_bytes()).unwrap(){
+        Some(value) => value,
+        None => return None,
+    };
     let res_url = std::str::from_utf8(&caps[1]).unwrap();
     match res_url {
         "/pgc/player/api/playurl" => Some(1),
